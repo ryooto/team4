38232a73747fafd7d90fa2eb1fd1affaffde326d
kagaaaaa <20040@hakodate.kosen-ac.jp>
2024-11-25 14:01:10 +0900

    トナカイ仮

--- game4/D.java
+++ game4/D.java
@@ -4,7 +4,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Write a description of class D here.
  * 
  * @author (your name) 
- * @version (a version number or a date)
+ * @version (a version number
+ or a date)
  */
 public class D extends Actor
 {
---
+++ game4/tonakai.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class tonakai here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class tonakai extends Actor
+{
+    int a=100;
+     setLocation(a);
+}
