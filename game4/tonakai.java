5ae11734c67d24bab33142b1cfe64e902389dc68
kagaaaaa <20040@hakodate.kosen-ac.jp>
2024-12-02 13:56:12 +0900

    tonakai1202

--- game4/tonakai.java
+++ game4/tonakai.java
@@ -1,3 +1,4 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -9,20 +10,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class tonakai extends Actor
 {
     private GreenfootImage img_bkup = null;
-      private int width=50;
-      private int height= 50;
+      private int width= 100;
+      private int height= 100;
 
       public tonakai()
       {
                img_bkup = new GreenfootImage( getImage() );
                getImage().scale( width,height );
-      }
-    
-      public void act() 
-      {
+               
                GreenfootImage img = new GreenfootImage(img_bkup);
                // (注意) scale で画像サイズを変更してから setImage すること
-               img.scale( width++,height++ );
+               img.scale( width,height );
                setImage(img);
-      } 
+      }
+    
+    public void act() 
+      {
+               move(-3);
+      }
+    
 }
