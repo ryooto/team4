f3cb94b3ae3f45ad1084befd1a7370998092b142
masarunba <20041@hakodate.kosen-ac.jp>
2024-12-02 13:47:39 +0900

    Dが鳥を透過

--- game4/bird.java
+++ game4/bird.java
@@ -11,20 +11,13 @@ public class bird extends Actor
     public bird(){
         GreenfootImage image = getImage();
         image.scale(512, 900);
+        getImage().scale( 100, 100 );
+        
     }
     
     public void act() 
     {
         // Add your action code here.
-        if(Jugador.isAlive()){
-            setLocation(getX() - 1, getY());
-        }
-        if(getX() <= 1){
-            setLocation(getX() + 700, 75 + Greenfoot.getRandomNumber(225));
-        }
-        if (Greenfoot.isKeyDown("r"))
-        {
-            getWorld().removeObject(this);
-        }
+        
     }
 }
