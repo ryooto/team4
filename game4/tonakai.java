c063a05c2eaa048b96e0571a72e43868c4307ba1
kagaaaaa <20040@hakodate.kosen-ac.jp>
2024-12-09 14:05:23 +0900

    tonakaiaaaaa

--- game4/tonakai.java
+++ game4/tonakai.java
@@ -22,6 +22,10 @@ public class tonakai extends Actor
                Actor actor = getOneIntersectingObject( sigekuni_run.class );
                if( actor != null ){
                    Greenfoot.stop();// sigekuni とぶつかった時の処理を書く
-                }  
+               }
+               Actor tonakai = getOneIntersectingObject(tonakai.class);
+               if(tonakai != null){
+                   getWorld().removeObject( tonakai );
+               }
       }
 }
